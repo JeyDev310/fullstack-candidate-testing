abc3385f3bad61b024e1c534e9dfc5f8666eb7b8
alextimdev <alextim1218@gmail.com>
2021-02-22 23:35:07 +0000

    update main

--- pages/api/jobs.js
+++ pages/api/jobs.js
@@ -1,7 +1,6 @@
 import jobs from '../../data/jobs'
 
 export default async (req, res) => {
-  const query = JSON.parse(req.body);
   res.statusCode = 200
   // @todo: implement filters and search
   // @todo: implement automated tests
@@ -11,26 +10,5 @@ export default async (req, res) => {
   // correct results even if server-side can't finish replies in the right order
   await new Promise((resolve) => setTimeout(resolve, 1000 * Math.random()))
 
-  const searched = jobs.filter(job => {
-    if (query.q) {
-      return job.name.toLowerCase.includes(query.q.toLowerCase())
-    }
-    return true;
-  })
-  let sorted = searched
-
-  const sortAry = (data, prop, isAsc) => {
-    if (!data[prop])
-      return data;
-
-    return data.sort((a, b) => {
-        return (a[prop] < b[prop] ? -1 : 1) * (isAsc ? 1 : -1)
-    });
-  }
-  
-  query.sorts.map(sort=> {
-    sorted = sortAry(sorted, sort.key, sort.way == 'asc')
-  })
-
-  res.json(sorted)
+  res.json({jobs: jobs})
 }
